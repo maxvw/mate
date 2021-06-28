e43e44b62e4072bd5777d923484706c2d21bb53b
Max <code@workwithmax.com>
2021-06-28 17:17:54 +0200

    writability test already happened in previous step

--- lib/mate/steps/deploy/unarchive_release.ex
+++ lib/mate/steps/deploy/unarchive_release.ex
@@ -5,18 +5,6 @@ defmodule Mate.Step.UnarchiveRelease do
   def run(%{remote: remote} = session) do
     archive_name = Path.basename(session.assigns.release_archive)
 
-    test_writable = """
-    #!/usr/bin/env bash
-    set -euo pipefail
-    mkdir -p "#{remote.release_path}"
-    cd "#{remote.release_path}"
-    touch test-write
-    rm test-write
-    """
-
-    with {:error, _error} <- Mate.remote_script(session, test_writable),
-         do: bail("Deploy user not allowed to write to #{remote.release_path}")
-
     script = """
     #!/usr/bin/env bash
     set -euo pipefail
